98f035405f15d17e7f373751de3b4a22155e346b
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-02-27 10:17:28 +0100

    added sm new code
    line 59 in JS not working

--- js/main.js
+++ js/main.js
@@ -1,62 +1,124 @@
 
 document.getElementById("mainTitle").innerText = "point and click adventure game";
 
-const gameWindow = document.getElementById("gameWindow");
+// Game State
+let gameState = {
+    "inventory": [],
+    "coinPickedUp": false
+}
+runGame();
+function runGame() {
+    //inventory
+    const gameWindow = document.getElementById("gameWindow");
+    const inventoryList = document.getElementById("inventoryList")
 
-const mainCharacter = document.getElementById("maincharacter")
-const offsetCharacter = 16;
+    //main character
+    const mainCharacter = document.getElementById("maincharacter")
+    const offsetCharacter = 16;
 
-const test1 = document.getElementById("squareTest");
-
-//inventory
-const inventoryList = document.getElementById("inventoryBox")
+    const test1 = document.getElementById("squareTest");
 
 
-gameWindow.onclick = function (e) {
-    var rect = gameWindow.getBoundingClientRect();
-    var x = e.clientX - rect.left;
-    var y = e.clientY - rect.top;
+    //inventory
+    //const inventoryList = document.getElementById("inventoryBox")
 
-    console.log("x is " + x + " and y is " + y);
 
-    if (e.target.id !== "maincharacterImg") {
-        mainCharacter.style.left = x - offsetCharacter + "px";
-        mainCharacter.style.top = y - offsetCharacter + "px";
-    }
+    gameWindow.onclick = function (e) {
+        var rect = gameWindow.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
 
-    /*if (e.target.id == "squareTest") {
-        test1.style.opacity = 0.5;
-    } else {
-        test1.style.opacity = 1;
-    }*/
+        //console.log("x is " + x + " and y is " + y);
 
-    switch (e.target.id) {
-        case "squareTest":
+        if (e.target.id !== "maincharacterImg") {
+            mainCharacter.style.left = x - offsetCharacter + "px";
+            mainCharacter.style.top = y - offsetCharacter + "px";
+        }
+
+        /*if (e.target.id == "squareTest") {
             test1.style.opacity = 0.5;
-        case "key":
-            ShowItem("rusty key", "rustyKey")
-            break
-        default:
+        } else {
             test1.style.opacity = 1;
+        }*/
+
+        switch (e.target.id) {
+            case "key":
+                console.log("pick up key")
+                document.getElementById("key").remove();
+                changeInventory('key', "add");
+                break;
+            case "well":
+                if (gameState.coinPickedUp == false) {
+                    changeInventory("coin", "add");
+                    gameState.coinPickedUp = true;
+                } else {
+                    console.log("There are no more coins in this well!");
+                }
+                break;
+            case "doorWizardHut":
+                if (checkItem("key")) {
+                    console.log("I opened the door. Yeah!");
+                } else if (checkItem("coin")) {
+                    changeInventory("coin", "remove");
+                    console.log("Oh no I lost the coin and it didn't open the door.. Feel kinda stupid..");
+                } else {
+                    console.log("Fuck this door is locked and I don't have a key. boohoo :(");
+                }
+                break;
+            case "statue":
+                console.log("hey you.. wanna know where the key is? It's by the graves.");
+                break;
+            default:
+                break;
+        }
     }
 
+    /**
+     * 
+     * @param {string} itemName 
+     * @param {string} action 
+     */
 
-    function GetItem(itemName, itemId) {
 
+    function changeInventory(itemName, action) {
+        if (itemName == null || action == null) {
+            console.error("Wrong parameters given to changeInventory()");
+            return;
+        }
+
+        switch (action) {
+            case 'add':
+                gameState.inventory.push(itemName);
+                break;
+            case 'remove':
+                gameState.inventory = gameState.inventory.filter(function (newInventory) {
+                    return newInventory !== itemName;
+                });
+                document.getElementById("inv-" + itemName).remove();
+                break;
+
+        }
+        updateInventory(gameState.inventory, inventoryList);
     }
 
-    function CheckItem() {
+    /**
+     * 
+     * @param {string} itemName 
+     */
 
+    function CheckItem(itemName) {
+        return gameState.inventory.includes(itemName);
     }
 
-    function ShowItem(itemName, itemId) {
-        console.log("you found a" + itemName);
-        const keyElement = document.createElement("li");
-        keyElement.id = itemId;
-        keyElement.innerText = itemName;
-        inventoryList.appendChild(keyElement);
+    function updateInventory(inventory, inventoryList) {
+        inventoryList.innerHTML = '';
+        inventory.forEach(function (item) {
+            const inventoryItem = document.createElement("li");
+            inventoryItem.id = 'inv-' + item;
+            inventoryItem.innerText = item;
+            inventoryList.appendChild(inventoryItem);
+        })
     }
-
 
 
 }
